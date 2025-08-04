bc32ae640d832721f0d054c5a0b3800a41876d81
Arittro7 <arittrowpdev@gmail.com>
2025-08-04 15:34:14 +0600

    URL added

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 const corsOptions = {
-  origin: 'https://bdgentask5.netlify.app', 
+  origin: 'https://bdgentask5.netlify.app',
   optionsSuccessStatus: 200
 };
 
 app.use(cors(corsOptions));
 app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Book Generator API is running successfully!');
+});
+// ------------------------------
 
 app.get('/api/books', (req, res) => {
   const { locale = 'en_US', seed = 123, page = 1, likesError = 0, reviewsError = 0 } = req.query;
